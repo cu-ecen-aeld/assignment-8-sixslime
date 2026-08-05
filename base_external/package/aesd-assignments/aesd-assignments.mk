32be30af094c0a7977221330d0dbbb8316c84884
SixSlime <ryan.mm.taylor@gmail.com>
2026-08-04 20:25:00 -0700

    update git hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO;DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = bd480555911b70e07cdb0c96ab3ccd3ff095b204
+AESD_ASSIGNMENTS_VERSION = 177bcc00f59d998f8ac71d48bd9e7c4d39820d54
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = bd480555911b70e07cdb0c96ab3ccd3ff095b204
+AESDCHAR_VERSION = 177bcc00f59d998f8ac71d48bd9e7c4d39820d54
 AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sixslime.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
